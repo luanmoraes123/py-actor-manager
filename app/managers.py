354ee5e0149d52fd4c073531c4ea4207f0c31d55
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-06-09 10:45:24 -0300

    final solution

--- app/managers.py
+++ app/managers.py
@@ -7,10 +7,10 @@ class ActorManager:
     def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
-        self.conn = sqlite3.connect(db_name)
-        self.cur = self.conn.cursor()
 
     def create(self, first_name: str, last_name: str) -> Actor:
+        self.conn = sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
         self.cur.execute(
             f'INSERT INTO {self.table_name} '
             f'(first_name, last_name) VALUES (?, ?)',
@@ -18,9 +18,12 @@ class ActorManager:
         )
         self.conn.commit()
         actor_id = self.cur.lastrowid
+        self.close_conn()
         return Actor(id=actor_id, first_name=first_name, last_name=last_name)
 
     def all(self) -> list[Actor]:
+        self.conn = sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
         self.cur.execute(f'SELECT * FROM {self.table_name}')
         rows = self.cur.fetchall()
 
@@ -28,19 +31,29 @@ class ActorManager:
         for row in rows:
             actor = Actor(id=row[0], first_name=row[1], last_name=row[2])
             actors.append(actor)
-
+        self.close_conn()
         return actors
 
     def update(self, pk: int, new_first_name: str,
                new_last_name: str) -> Actor:
+        self.conn = sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
         self.cur.execute(
             f'UPDATE {self.table_name} '
             f'SET first_name = ?, last_name = ? WHERE id = ?',
             (new_first_name, new_last_name, pk)
         )
         self.conn.commit()
+        self.close_conn()
         return Actor(id=pk, first_name=new_first_name, last_name=new_last_name)
 
     def delete(self, pk: int) -> None:
+        self.conn = sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
         self.cur.execute(f'DELETE FROM {self.table_name} WHERE id = ?', (pk,))
         self.conn.commit()
+        self.close_conn()
+
+    def close_conn(self) -> None:
+        self.cur.close()
+        self.conn.close()
